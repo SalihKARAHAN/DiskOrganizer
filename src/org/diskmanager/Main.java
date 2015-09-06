e195f065cf0f5d3f87dee2d1dbc84ab9c0e8ddf5
Salih KARAHAN <salihkarahan@outlook.com>
2015-09-07 00:46:55 +0300

    Initialize project with sample javafx

---
+++ src/org/diskmanager/Main.java
@@ -0,0 +1,36 @@
+package org.diskmanager;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+//			Button fxButtonLogin = new Button();
+//			fxButtonLogin.setText("Login");
+			
+			Parent root = FXMLLoader.load(getClass().getResource("view/LoginView.fxml"));
+			
+//			root.getChildren().add(fxButtonLogin);
+			
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/org/diskmanager/controller/AuthController.java
@@ -0,0 +1,15 @@
+package org.diskmanager.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class AuthController {
+	@FXML
+	private Text actiontarget;
+
+	@FXML
+	protected void handleSubmitButtonAction(ActionEvent event) {
+		actiontarget.setText("Sign in button pressed");
+	}
+}
