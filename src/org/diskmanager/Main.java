c80b8d312f176713dc0653a1540b245a29357af8
Salih KARAHAN <salihkarahan@outlook.com>
2015-09-09 01:06:33 +0300

    Sync other files

--- src/org/diskmanager/Main.java
+++ src/org/diskmanager/Main.java
@@ -8,23 +8,34 @@ import javafx.scene.Scene;
 //import javafx.scene.control.Button;
 //import javafx.scene.layout.AnchorPane;
 
-
 public class Main extends Application {
+	
+	private Environment _environment;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			
+			Setup setup = new Setup();
+			setup.CheckSystem();
+			
 //			Button fxButtonLogin = new Button();
 //			fxButtonLogin.setText("Login");
 			
-			Parent root = FXMLLoader.load(getClass().getResource("view/LoginView.fxml"));
+//			Parent root = FXMLLoader.load(getClass().getResource("view/LoginView.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("view/MainView.fxml"));
 			
 //			root.getChildren().add(fxButtonLogin);
 			
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Disk Organizer");
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
