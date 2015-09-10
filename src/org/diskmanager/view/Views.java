cc2d52a0cb39d96d935906c9cf63c6f84e65d864
Salih KARAHAN <salihkarahan@outlook.com>
2015-09-10 19:08:50 +0300

    This class keep all fxml page infos

---
+++ src/org/diskmanager/view/Views.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.diskmanager.view;
+
+/**
+ * @author Salih.KARAHAN
+ *
+ */
+public final class Views {
+	
+	public javafx.stage.Stage GetView(String viewName){
+		return null;
+	}
+}
