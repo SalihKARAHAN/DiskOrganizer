5bb284dfa5e44e5d9a90fa0e9e72b2321069fdc7
Salih KARAHAN <salihkarahan@outlook.com>
2015-09-11 12:33:36 +0300

    MainView content updated

--- src/org/diskmanager/controller/MainController.java
+++ src/org/diskmanager/controller/MainController.java
@@ -8,5 +8,22 @@ package org.diskmanager.controller;
  *
  */
 public class MainController {
+	@javafx.fxml.FXML
+	private javafx.scene.control.TreeView<String> fxCategoryViewer;
 
+	// the initialize method is automatically invoked by the FXMLLoader - it's
+	// magic
+	public void initialize() {
+		loadTreeItems("initial 1", "initial 2", "initial 3");
+	}
+
+	public void loadTreeItems(String... rootItems) {
+		javafx.scene.control.TreeItem<String> root = new javafx.scene.control.TreeItem<String>("Root Node");
+		root.setExpanded(true);
+		for (String itemString : rootItems) {
+			root.getChildren().add(new javafx.scene.control.TreeItem<String>(itemString));
+		}
+
+		fxCategoryViewer.setRoot(root);
+	}
 }
