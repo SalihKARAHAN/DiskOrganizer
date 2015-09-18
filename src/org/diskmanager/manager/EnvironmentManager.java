57c2e6e006ec5d6ea56e0a1cad1c791ff6f5dd78
Salih KARAHAN <salihkarahan@outlook.com>
2015-09-18 09:43:51 +0300

    change namespace

---
+++ src/org/diskmanager/manager/EnvironmentManager.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.diskmanager.manager;
+
+import org.diskmanager.schema.Environment;
+
+/**
+ * @author Salih.KARAHAN
+ *
+ */
+public class EnvironmentManager {
+	
+	public static Environment GetEnvironment(){
+		return new Environment();
+	}
+
+}
